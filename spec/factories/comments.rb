f4bb977526d24a270a02975f0fc55b8399c24361
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-03 22:41:15 +0500

    fix comment facotry file

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    body { 'This is a comment.' }
+    text { 'This is a comment.' }
     association :author, factory: :user
     association :post, factory: :post
   end
