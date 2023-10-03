34300fa66986b0cdd487da400287f4c4b84a5e56
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-10-03 23:20:21 +0500

    Modify files

--- spec/integeration/userposts_spec.rb
+++ spec/integeration/userposts_spec.rb
@@ -22,8 +22,8 @@ RSpec.feature 'User post index page', type: :feature do
       expect(page).to have_content(post.title)
       expect(page).to have_content(post.text.truncate(50))
       expect(page).to have_content(post.comments.first.body) if post.comments.any?
-      expect(page).to have_content("Comments #{post.comments.count}")
-      expect(page).to have_content("Likes #{post.likes.count}") # Truncate post body for visibility
+      expect(page).to have_content("Comments: #{post.comments.count}")
+      expect(page).to have_content("Likes: #{post.likes.count}") # Truncate post body for visibility
     end
 
     # Check if the first comments on a post are visible
@@ -34,7 +34,7 @@ RSpec.feature 'User post index page', type: :feature do
     expect(page).to have_css('.pagination')
 
     # Click on a post and check if it redirects to the post's show page
-    first('h4 a').click
+    first('ul a').click
     expect(page).to have_current_path(user_post_path(user, user.posts.first))
   end
 end
--- spec/integeration/usershow_spec.rb
+++ spec/integeration/usershow_spec.rb
@@ -23,16 +23,16 @@ RSpec.feature 'User show page', type: :feature do
 
     # Check if the first 3 posts are visible
     user.posts.first(3).each_with_index do |post, index|
-      expect(page).to have_content("Post ##{index + 1}")
+      expect(page).to have_content(post.title)
       expect(page).to have_content(post.text.truncate(50)) # Truncate post body for visibility
     end
 
     # Check if the "View All Posts" button is visible
-    expect(page).to have_link('View All Posts', href: user_posts_path(user))
+    expect(page).to have_link('See all posts', href: user_posts_path(user))
   end
 
   scenario 'User clicks on a user post and is redirected to its show page' do
-    first('h4 a').click
+    first('ul a').click
 
     # Check if the page is redirected to the post's show page
     expect(page).to have_current_path(user_post_path(user, user.posts.first))
@@ -40,7 +40,7 @@ RSpec.feature 'User show page', type: :feature do
 
   scenario 'User clicks on "View All Posts" and is redirected to the user post index page' do
     # Click on the "View All Posts" button
-    click_on 'View All Posts'
+    click_on 'See all posts'
 
     # Check if the page is redirected to the user's post index page
     expect(page).to have_current_path(user_posts_path(user))
