86c981f6295c4fa3ac418a5c3e6607ab598e5e04
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-10-05 12:48:40 +0500

    fix linters

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,17 +2,10 @@ require 'rails_helper'
 
 RSpec.feature 'Logins', type: :feature do
   background { visit new_user_session_path }
-  scenario 'displays email field' do
-    expect(page).to have_field('user[email]')
-  end
 
-  scenario 'displays password field' do
-    expect(page).to have_field('user[password]')
-  end
-
-  scenario 'displays email field' do
-    expect(page).to have_button('Log in')
-  end
+  scenario('display email field') { expect(page).to have_field('user[email]') }
+  scenario('displays password field') { expect(page).to have_field('user[password]') }
+  scenario('displays email field') { expect(page).to have_button('Log in') }
 
   context 'Form Submission' do
     scenario 'Submit form without email and password' do
